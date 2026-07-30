d8c5d8006ad63be1b75e5ae23ae7ccee7c3d9170
Amruta Haramalkar <haramalkaramruta@gmail.com>
2026-07-30 10:56:37 +0530

    Time: 2 ms (99.31%), Space: 47.1 MB (59.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -10,14 +10,7 @@ class Solution {
                 return new int[] {map.get(comp),i}; 
             }
             map.put(nums[i],i);
-            // for(int i=0;i<nums.length;i++){
-        //     for (int j = i + 1; j < nums.length; j++) {
-        //         if (nums[i] + nums[j] == target) {
-        //             return new int[] { i, j };
-        //         }
-        //     }
-        // }
-        // return new int[] {};
+            
         }
         return new int[] {};
     }

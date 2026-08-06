c334c88de05a9fcdc37f9a41de1ecd301cb756cd
Amruta Haramalkar <haramalkaramruta@gmail.com>
2026-08-06 19:01:33 +0530

    Time: 0 ms (100%), Space: 42.9 MB (62.96%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,34 +1,47 @@
-
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int left = 0;
-        int right = matrix[0].length-1;
-        int top = 0;
-        int bottom = matrix.length-1;
-        int k=matrix[0].length * matrix.length;
-        List<Integer> numbers = new ArrayList<>();
-        while(left<=right && top<=bottom){
-            for(int i = left;  i<=right && k!=0; i++){
-                numbers.add(matrix[top][i]);
-                k--;
+        int m=matrix.length;
+        int n=matrix[0].length;
+        List<Integer> result=new ArrayList<>(); 
+
+        int startingRow=0;
+        int endingRow=m-1;
+        int startingCol=0;
+        int endingCol=n-1;
+
+        while(startingRow<=endingRow && startingCol<=endingCol){
+            //row wise left to right-->startingRow print krni hai,from startingCol to endingCol
+            for(int col=startingCol;col<=endingCol;col++){
+                result.add(matrix[startingRow][col]);
             }
-            top++;
-            for(int i = top; i<=bottom&& k!=0 ; i++){
-                numbers.add(matrix[i][right]);
-                k--;
+            startingRow++;
+
+
+            //col wise top to bottom-->endingCol print krni hai, from startingRow to endingRow
+            for(int row=startingRow;row<=endingRow;row++){
+                result.add(matrix[row][endingCol]);
             }
-            right--;
-            for(int i = right;  i>=left&& k!=0; i--){
-                numbers.add(matrix[bottom][i]);
-                k--;
+            endingCol--;
+
+            //row wise right to left-->endingRow print krni hai, from endingCol to startingCol
+            //valid endingRow-->means startingRow<=endingRow
+            if(startingRow<=endingRow){
+                for(int col=endingCol;col>=startingCol;col--){
+                    result.add(matrix[endingRow][col]);
+                }
+                endingRow--;
             }
-            bottom--;
-            for(int i = bottom; i>=top&& k!=0 ; i--){
-                numbers.add(matrix[i][left]);
-                k--;
-            }
-            left++;
-        }
-        return numbers;
+            
+
+            //col wise botton to top-->startingCol print krni hai to ,from endingRow to startingRow
+            //valid endingRow-->means startingCol<=endingCol
+            if(startingCol<=endingCol){
+                for(int row=endingRow;row>=startingRow;row--){
+                    result.add(matrix[row][startingCol]);
+                }
+                startingCol++;
+            }  
+        }  
+        return result;   
     }
 }
